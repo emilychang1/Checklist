972c6f51d4ab390d6d579656ca0d487f3e39a539
Emily Chang <echang7@berkeley.edu>
2017-07-19 00:25:50 -0700

    basic list

---
+++ Checklist/ItemTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ItemTableViewCell.swift
+//  Checklist
+//
+//  Created by Emily Chang on 7/18/17.
+//  Copyright © 2017 Emily Chang. All rights reserved.
+//
+
+import Foundation
---
+++ ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  Checklist
+//
+//  Created by Emily Chang on 7/18/17.
+//  Copyright © 2017 Emily Chang. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
